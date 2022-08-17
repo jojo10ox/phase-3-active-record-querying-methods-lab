f24298bd42dacbab2e8c49acc2a8210a6c851224
Josephine Luu <josephinegluu@gmail.com>
2022-08-16 20:25:54 -0500

    commit tree d9955c81a8f9ede0d0d5be9514ddbdddbb7ba8db

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+puts "seeding"
+
+Show.create(name: "vampire diaries", network: "cw", day: "monday", rating: 10, season: "fall")
+Show.create(name: "the oc", network: "cw", day: "tuesday", rating: 8, season: "winter")
+
+
+
+puts"done seeding"
